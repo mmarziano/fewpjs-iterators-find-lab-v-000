da4eaf852d3b4ff51aec19023e57617ef3ab8f4c
Monica Marziano <mmarziano1@gmail.com>
2019-10-22 13:09:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let win = arr.find( record => record.result === 'W' )
+  if (win) {
+    return win.year;
+    }
+  
+}
